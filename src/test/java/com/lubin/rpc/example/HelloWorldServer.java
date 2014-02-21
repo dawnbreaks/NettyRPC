7487750eda4c14114def37a58a3de978494738a0
lubinwang <308181687@qq.com>
2014-02-21 17:02:42 +0800

    add  test folder

---
+++ src/test/java/com/lubin/rpc/example/HelloWorldServer.java
@@ -0,0 +1,10 @@
+package com.lubin.rpc.example;
+
+import com.lubin.rpc.server.RPCServer;
+
+public class HelloWorldServer {
+
+	public static void main(String[] args) throws Exception {
+		new RPCServer().run();
+	}
+}
