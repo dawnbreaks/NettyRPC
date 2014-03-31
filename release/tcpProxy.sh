8aad5068e470c9b608551fa6fdf17bd17eafc107
lubinwang <308181687@qq.com>
2014-03-31 17:37:56 +0800

    update script

--- release/tcpProxy.sh
+++ release/tcpProxy.sh
@@ -2,10 +2,10 @@
 pid=`ps -ef|grep java|grep tcpproxy|awk '{print $2}'`
 if [ "x$pid" != "x" ];
 then
-    echo  "kill service process." 
+    echo  "killing service process." 
     kill  -9 $pid
     sleep  4
 fi
 
-echo "start service....."
+echo "starting service....."
 nohup java -cp ./dependency/*:./*   com.lubin.tcpproxy.TcpProxyServer  2>&1 >> ./tcpProxy.log  &
